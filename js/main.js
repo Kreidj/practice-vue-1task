16d4c37022ffa6fe101e00f190de513cc7d1fbac
Kreidj <sechenov97@gmail.com>
2024-02-12 11:08:38 +0700

    6 Практическая работа

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ let app = new Vue({
         product: "Socks",
         image: "./assets/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
-        inStock: true,
+        inStock: false,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
@@ -18,23 +18,14 @@ let app = new Vue({
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
             }
         ],
-
-        cart: 0,
-        cartValue:0,
+        cart: 0
     },
     methods: {
-        addOneToCart(){
-            this.cartValue +=1
-        },
-        minusOneToCart(){
-            this.cartValue -=1
-        },
         addToCart() {
-            this.cart += this.cartValue
+            this.cart += 1
         },
         updateProduct(variantImage) {
             this.image = variantImage
-        },
-
+        }
     }
 })
