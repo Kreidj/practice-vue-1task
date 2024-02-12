d3ffe02fff891bab19bf08649f0fc5dfb80204e4
Kreidj <sechenov97@gmail.com>
2024-02-12 11:49:15 +0700

    7 Практическая работа

--- js/main.js
+++ js/main.js
@@ -2,30 +2,53 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        image: "./assets/vmSocks-green-onWhite.jpg",
+        brand: 'Vue Mastery',
+        selectedVariant: 0,
         altText: "A pair of socks",
-        inStock: false,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        onSale: true,
         variants: [
             {
                 variantId: 2234,
                 variantColor: 'green',
                 variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10,
+                variantOnSale: true,
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0,
+                variantOnSale: false,
             }
         ],
+
         cart: 0
     },
     methods: {
         addToCart() {
             this.cart += 1
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
+        updateProduct(index) {
+            this.selectedVariant = index;
+            console.log(index);
+        }
+
+    },
+    computed: {
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        sale(){
+            return this.brand + ' ' + this.product + ' Sale! '
         }
     }
+
 })
